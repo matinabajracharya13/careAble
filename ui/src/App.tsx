7238c1dccfe6b0b4000e4f743012697c96292b47
matinabajracharya13 <22252274@students.latrobe.edu.au>
2026-03-27 11:55:38 +1100

    initial commit

---
+++ ui/src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div className='min-h-screen bg-background'>
+      {/* Navbar */}
+      <h1 className='text-2xl font-bold p-4'>Welcome to Cartable</h1>
+    </div>
+  );
+}
